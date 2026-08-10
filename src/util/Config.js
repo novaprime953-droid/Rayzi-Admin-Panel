1f6289b11bff6aa18b3f4f78363c6e5b0f810e69
socialvibestudiopk-source <socialvibestudiopk@gmail.com>
2026-08-11 00:34:43 +0500

    Hardcode Backend URL for Netlify fix

--- src/util/Config.js
+++ src/util/Config.js
@@ -1,3 +1,3 @@
-exports.baseURL = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000";
+exports.baseURL = "https://rayzi-backend-ldh1.onrender.com";
 
 exports.key = "Lumora_Audio_2023_Secure_Key";
