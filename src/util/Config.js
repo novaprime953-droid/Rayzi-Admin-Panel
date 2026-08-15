82540b0ce1f0e9ee982e273899829d59a0f658b9
socialvibestudiopk-source <socialvibestudiopk@gmail.com>
2026-08-15 13:27:04 +0500

    Configure frontend to use REACT_APP_BACKEND_URL environment variable

--- src/util/Config.js
+++ src/util/Config.js
@@ -1,3 +1,3 @@
-exports.baseURL = "https://rayzi-backend-ldh1.onrender.com";
+exports.baseURL = process.env.REACT_APP_BACKEND_URL || "https://rayzi-backend-ldh1.onrender.com";
 
 exports.key = "Lumora_Audio_2023_Secure_Key";
